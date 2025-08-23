91f3b197b85a32a05a831e7c5bbd80668962db42
Mohatamim Haque <mohatamimhaque@outlook.com>
2025-08-23 11:10:50 +0600

    Add/update 2009A_Minimize_.cpp

---
+++ solutions/codeforces/2009A_Minimize_.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+ 
+void init_code(){
+    fast_io;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+}
+
+int main(){
+    //init_code();
+    int a,b,t;
+    cin>>t;
+    while(t--){
+        cin>>a>>b;
+        cout<<b-a<<endl;
+    }
+}
