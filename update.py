1dd4e5fe9ce1c1ae6f45b3052b1d5e6029716ada
Mohatamim Haque <mohatamimhaque@outlook.com>
2025-08-23 10:52:49 +0600

    Update repository

--- update.py
+++ update.py
@@ -2,7 +2,7 @@ import requests
 import datetime
 
 handle = "nocturnalLogic"  # Codeforces handle
-url = f"https://codeforces.com/api/user.status?handle={handle}&from=1&count=10000"  
+url = f"https://codeforces.com/api/user.status?handle={handle}&from=1&count=200000"  
 
 res = requests.get(url).json()
 
@@ -18,7 +18,7 @@ def get_extension(language: str) -> str:
         return ".cs"
     if "java" in language:
         return ".java"
-    if "python 3" in language or "python 2" in language:
+    if "py" in language or "python 2" in language:
         return ".py"
     if "pascal" in language:
         return ".pas"
@@ -36,14 +36,18 @@ def get_extension(language: str) -> str:
 
 submissions = {}
 
+dup = set()
+
 # Build dictionary
 for sub in res["result"]:
+    if(sub['verdict'] != 'OK'):
+        continue
     pid = sub["id"]
     contest_id = sub.get("contestId", 0)
     submissions[pid] = {
         "contest_id": contest_id,
         "index": sub["problem"]["index"],
-        "name": sub["problem"]["name"],
+        "name": sub["problem"]["name"].replace("'", "").replace('"', ""),
         "language": sub["programmingLanguage"],
         "tags": ", ".join(sub['problem'].get('tags', [])),
         "submitted_time": sub['creationTimeSeconds'],
@@ -54,12 +58,21 @@ for sub in res["result"]:
 # Sort by submission time (latest first)
 sorted_subs = sorted(submissions.items(), key=lambda x: x[1]['submitted_time'], reverse=True)
 
+
 # Build README
 readme = "# 🏆 Codeforces Submissions\n\n"
 readme += "| # | Platform | Problem | Solution | Tags | Lang | Submitted | Time | Memory |\n"
 readme += "|---|----------|---------|----------|------|------|-----------|------|--------|\n"
 
+
+
+dup = set()
+
 for i, (pid, data) in enumerate(sorted_subs, 1):
+    if((data['contest_id'],data["index"]) in dup):
+        continue
+
+    dup.add((data['contest_id'],data["index"]))
     problem_link = f'<a href="https://codeforces.com/contest/{data["contest_id"]}/problem/{data["index"]}" target="_blank"> {data['contest_id']}{data["index"]} - {data["name"]} </a>'
     sub_link = f'<a href="solutions/codeforces/{data['contest_id']}{data['index']}_{data["name"].replace(" ", "_")}{get_extension(data["language"])}" target="_blank">Solutions</a>'
     platform = "Codeforces"
