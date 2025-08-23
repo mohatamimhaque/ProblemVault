f1e758e8ee6ddb2bfe60ec06d2258e59d2c0b1cb
Mohatamim Haque <mohatamimhaque@outlook.com>
2025-08-23 15:12:33 +0600

    Add/update 1760A_Medium_Number.cpp

--- solutions/codeforces/1760A_Medium_Number.cpp
+++ solutions/codeforces/1760A_Medium_Number.cpp
@@ -3,28 +3,24 @@
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
 using namespace std;
 
-void init_code() {
+void init_code(){
     fast_io;
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     #endif
 }
-int main() {
-    //init_code();
-    int t,i=1,m,a,b,c,min,max;
+
+int main(){
+   //init_code();
+    int t,x,y,z,a,b,c;
+    string str,out;
     cin>>t;
     while(t--){
-        cin>>a>>b>>c;
-        min = a<b?a<c?a:c:b<c?b:c;
-        max = a>b?a>c?a:c:b>c?b:c;
-        cout<<(a+b+c) - (max+min)<<endl;
+        cin>>x>>y>>z;
+        a=x>y?x>z?x:z:y>z?y:z;
+        b=x<y?x<z?x:z:y<z?y:z;
+        c=x+y+z-a-b;
+        cout<<c<<endl;
     }
-    
-        
-        
-    
-  
-    
-    return 0;
 }
