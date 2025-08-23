387ff8f535cd6a61c85668ef8209f22f2a4b895d
Mohatamim Haque <mohatamimhaque@outlook.com>
2025-08-23 14:18:18 +0600

    Add/update 1742C_Stripes.cpp

---
+++ solutions/codeforces/1742C_Stripes.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define lli long long int
+#define ll long long
+#define nl '\n'
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define mod 1000000007
+using namespace std;
+
+void init_code() {
+    fast_io;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+ll bigMod(ll b,ll p){
+    ll res=1;
+    while(p){
+        if(p&1) res=(res*b)%mod;
+        b=(b%mod*b%mod)%mod;
+        p>>=1;
+    }
+    return res%mod;
+ }
+
+template <class T>
+void print(const T& a) {
+    for (const auto& d : a) {
+        cout << d << ' ';
+    }
+    cout <<nl;
+}
+template <class T>
+void sorting(T& a){
+    sort(a.begin(),a.end(), greater<ll>());
+}
+void vecIn(vector<ll>& a,ll n){
+    ll d;
+    for(ll i=0;i<n;i++){
+        cin>>d;
+        a.push_back(d);
+    }
+}
+void solve(){
+    int i,j,r=0;
+    char c;
+    bool color = false;
+    for(i=0;i<8;i++){
+        r=0;
+        for(j=0;j<8;j++){
+            cin>>c;
+            if(c=='R'){
+                r++;
+            }
+        }
+        if(r==8){
+            color = true;
+        }
+    }
+    if(color){
+        cout<<"R\n";
+        return;
+    }
+    cout<<"B\n";
+    
+}
+int main() {
+    init_code();
+
+    int t=1;
+    cin >> t;
+
+    while (t--) {
+       solve();
+    }
+
+    return 0;
+}
