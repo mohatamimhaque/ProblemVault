d1a0b3ffc166424a6e3ea1975bf7216b83254a9d
Mohatamim Haque <mohatamimhaque@outlook.com>
2025-08-23 10:03:52 +0600

    Add/update 112A_Petya_and_Strings.cpp

--- solutions/codeforces/112A_Petya_and_Strings.cpp
+++ solutions/codeforces/112A_Petya_and_Strings.cpp
@@ -1,42 +1,22 @@
 #include <bits/stdc++.h>
-#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL)
-#define lli long long int
-#define nl '\n'
-#define yes cout<<"YES\n"
-#define no cout<<"NO\n"
 using namespace std;
-
-void init_code() {
-    fast_io;
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-}
-
-int solve() {
-    string s1,s2;
-    cin>>s1>>s2;
-    for(int i=0;i<s1.size();i++){
-        if(tolower(s1[i])>tolower(s2[i])){
-            return 1;
-        }else if(tolower(s1[i])<tolower(s2[i])){
-            return -1;
-        }
-    }
-    
-    return 0;
-}
-
-int main() {
-    init_code();
-    
-    int t = 1;
-    //cin >> t;
-    
-    while (t--) {
-        cout<<solve()<<endl;
+int main()
+{
+    string first,second;
+    cin>>first;
+    cin>>second;
+    transform(first.begin(), first.end(), first.begin(), ::tolower);
+    transform(second.begin(), second.end(), second.begin(), ::tolower);
+    if(first<second){
+        cout<<-1;
+    }else if(first>second){
+        cout<<1;
+    }else{
+        cout<<0;
     }
 
-    return 0;
+    cout<<"\n";
+
+  
+  return 0;
 }
