49676b56742a400a77584732397581c0a698f81f
Mohatamim Haque <mohatamimhaque@outlook.com>
2025-08-23 11:10:58 +0600

    Add/update 2010B_Three_Brothers.cpp

---
+++ solutions/codeforces/2010B_Three_Brothers.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+ 
+void init_code(){
+    fast_io;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+}
+
+int main(){
+    //init_code();
+    int a,b;
+    cin>>a>>b;
+    cout<<6-(a+b)<<endl;
+}
