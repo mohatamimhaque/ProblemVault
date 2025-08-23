dfc7401863b2b8183d0a9e045c109530553f3fba
Mohatamim Haque <mohatamimhaque@outlook.com>
2025-08-23 07:23:30 +0600

    Add/update 1520C_Not_Adjacent_Matrix.cpp

--- solutions/codeforces/1520C_Not_Adjacent_Matrix.cpp
+++ solutions/codeforces/1520C_Not_Adjacent_Matrix.cpp
@@ -36,15 +36,8 @@ void solve(){
         cout<<-1<<nl;
         return;
     }
-    vector<int>vec;
     for(int i=1;i<=n*n;i+=2){
-        vec.push_back(i);
-    }
-    for(int i=2;i<=n*n;i+=2){
-        vec.push_back(i);
-    }
-    for(int i=0;i<n*n;i++){
-        cout<<vec[i];
+        cout<<i;
         if(tmp==n){
             cout<<'\n';
             tmp=1;
@@ -53,6 +46,17 @@ void solve(){
             tmp++;
         }
     }
+    for(int i=2;i<=n*n;i+=2){
+        cout<<i;
+        if(tmp==n){
+            cout<<'\n';
+            tmp=1;
+        }else{
+            cout<<' ';
+            tmp++;
+        }
+    }
+    
 
 
 
