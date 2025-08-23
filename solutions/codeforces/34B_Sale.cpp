027b0329d96adef9646973de5286dc6d3ab1e27f
Mohatamim Haque <mohatamimhaque@outlook.com>
2025-08-23 06:41:59 +0600

    Add/update 34B_Sale.cpp

---
+++ solutions/codeforces/34B_Sale.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define nl '\n'
+#define mod 1000000007
+#define int long long int
+#define uint unsigned long long
+using namespace std;
+
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+
+void solve(int t){
+    int n,m,i=0,res=0;
+    cin>>n>>m;
+    vector<int>vec(n);
+    cin>>vec;
+    sort(vec.begin(),vec.end());
+    while(i<m && vec[i]<0){
+        res+=abs(vec[i]);
+        i++;
+    }
+
+    cout<<res<<nl;
+
+
+    
+}
+
+int32_t main(){
+    fast_io;
+    //init_code();
+    int t = 1,i=1;
+    //cin >> t;
+    while (i<=t){
+        solve(i++);
+    }
+    return 0;
+}
