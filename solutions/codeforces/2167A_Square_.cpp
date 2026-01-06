4af259c7d8359a4bb06ec7284f3c33cc51a4fa47
Mohatamim Haque <mohatamimhaque@outlook.com>
2026-01-06 23:07:15 +0600

    Add/update 2167A_Square_.cpp

---
+++ solutions/codeforces/2167A_Square_.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int a,b,c,d;cin>>a>>b>>c>>d;
+
+        if(a==b && c==d && d==a && c==b){
+            cout<<"YES\n";
+        }else{
+            cout<<"NO\n";
+        }
+    }
+}
+
+
