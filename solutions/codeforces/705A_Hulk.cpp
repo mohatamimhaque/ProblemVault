bbfbc4ea83d8f878c67cc42b850d1a255c034edf
Mohatamim Haque <mohatamimhaque@outlook.com>
2026-03-13 22:38:36 +0600

    Add/update 705A_Hulk.cpp

--- solutions/codeforces/705A_Hulk.cpp
+++ solutions/codeforces/705A_Hulk.cpp
@@ -1,50 +1,65 @@
 #include <bits/stdc++.h>
-#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL)
-#define nl '\n'
-#define ps(b) cout << ((b) ? "YES\n" : "NO\n")
-#define mod 1000000007
-#define uint unsigned long long
 using namespace std;
+#define FAST_IO ios::sync_with_stdio(false);cin.tie(nullptr)
+#define MOD 1000000007LL
+#define nl '\n'
 
-template<typename T, typename U> istream &operator>>(istream &in, pair<T, U> &a) { return in >> a.first >> a.second; }
-template<typename T> istream &operator>>(istream &in, vector<T> &a) { for (auto &x : a) in >> x; return in; }
-template<typename T, typename U> ostream &operator<<(ostream &out, const pair<T, U> &a) { return out << a.first << ' ' << a.second; }
-template<typename T, typename U> ostream &operator<<(ostream &out, const vector<pair<T, U>> &a) { for (auto &x : a) out << x << '\n'; return out; }
-template<typename T> ostream &operator<<(ostream &out, const vector<T> &a) { int n = a.size(); if (!n) return out; out << a[0]; for (int i = 1; i < n; i++) out << ' ' << a[i]; return out; }
+using ll=long long;
+using ull=unsigned long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+using vc=vector<ll>;
 
-void init_code() {
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<"="<<x<<endl
+#define debugv(v) cerr<<#v<<"=";for(auto e:v) cerr<<e<<" ";cerr<<endl
+#else
+#define debug(x)
+#define debugv(v)
+#endif
+
+inline void init_file_io(){
+    #ifndef ONLINE_JUDGE 
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
     #endif
 }
 
-bool isPrime(int n) { return n == 2 || n == 3 ? true : (n <= 1 || n % 2 == 0 || n % 3 == 0 ? false : ([&]() { for (int i = 5; i * i <= n; i += 6) if (n % i == 0 || n % (i + 2) == 0) return false; return true; })()); }
-int digit(int n) { return n <= 0 ? 0 : 1 + digit(n / 10); }
-uint power(int x, int y, int p = mod) { uint res = 1; x %= p; while (y > 0) { if (y & 1) res = (res * x) % p; y >>= 1; x = (x * x) % p; } return res; }
-int modadd(int a, int b) { return ((a % mod) + (b % mod)) % mod; }
-int modsub(int a, int b) { return ((a % mod) - (b % mod) + mod) % mod; }
-int modmul(int a, int b) { return ((1LL * a % mod) * (b % mod)) % mod; }
-uint modInverse(int n, int p = mod) { return power(n, p - 2, p); }
+class cp {
+public:
+    ll a,b,n,m,x,y,l,k,r;
+    vector<ll>vec;
+    void solve(int t) {
+        int n;cin>>n;
+        cout<<"I hate";
+        for(int i=1;i<n;i++){
+            cout<<((i%2==0)?" that I hate":" that I love");
+        }
 
-void solve(int t) {
-    int n;cin>>n;
-    cout<<"I hate";
-    for(int i=2;i<=n;i++){
-        cout<<" that "<<(i&1?"I hate":"I love");
+        cout<<" it\n";
+        
     }
-    cout<<" it\n";
-}
+};
 
-int32_t main(){
-    fast_io;
-    init_code();
-    int t = 1, i = 1;
-    // cin >> t;
-    while (i <= t) {
-        solve(i++);
-    }
+int main() {
+    FAST_IO;
+    init_file_io();
+    cp obj;
+
+    int T=1;
+    // cin>>T;
+    for(int t=1; t<=T; t++) obj.solve(t);
+    
     return 0;
 }
 
-// Usage: Type "cptemplate" and press TAB (or your snippet expand key).
+
+/*
+  __  __       _           _                  _           
+ |  \/  |     | |         | |                (_)          
+ | \  / | ___ | |__   __ _| |_ __ _ _ __ ___  _ _ __ ___  
+ | |\/| |/ _ \| '_ \ / _` | __/ _` | '_ ` _ \| | '_ ` _ \ 
+ | |  | | (_) | | | | (_| | || (_| | | | | | | | | | | | |
+ |_|  |_|\___/|_| |_|\__,_|\__\__,_|_| |_| |_|_|_| |_| |_|
+                                                          
+*/
