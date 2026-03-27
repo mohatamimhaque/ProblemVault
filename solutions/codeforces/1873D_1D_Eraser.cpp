e968b33f0a2733699d1afb8181ad4815de4e3981
Mohatamim Haque <mohatamimhaque@outlook.com>
2026-03-27 15:59:31 +0600

    Add/update 1873D_1D_Eraser.cpp

--- solutions/codeforces/1873D_1D_Eraser.cpp
+++ solutions/codeforces/1873D_1D_Eraser.cpp
@@ -1,58 +1,71 @@
 #include <bits/stdc++.h>
-#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL)
-#define nl '\n'
-#define yes cout<<"YES\n"
-#define no cout<<"NO\n"
-#define mod 1000000007
-#define int long long int
-#define uint unsigned long long
 using namespace std;
+#define FAST_IO ios::sync_with_stdio(false);cin.tie(nullptr)
+#define MOD 1000000007LL
+#define nl '\n'
 
-template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
-template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
-template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
-template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
-template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+using ll=long long;
+using ull=unsigned long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+using vc=vector<ll>;
 
-void init_code() {
-    fast_io;
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<"="<<x<<endl
+#define debugv(v) cerr<<#v<<"=";for(auto e:v) cerr<<e<<" ";cerr<<endl
+#else
+#define debug(x)
+#define debugv(v)
+#endif
+
+inline void init_file_io(){
+    #ifndef ONLINE_JUDGE 
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
     #endif
-}    
-bool isPrime(int n){if(n==1) return false;if(n==2 || n==3) return true;if(n%2==0 || n%3==0) return false;for(int i=5;i*i<=n;i+=6){if(n%i==0 || n%(i+2)==0) return false;}return true;}
-int digit(int n) {if(n <= 0) {return 0;}return 1 + digit(n / 10);} 
-uint power(int x, int y, int p =  mod){unsigned long long res = 1;x = x % p;while (y > 0){if (y & 1)res = (res * x) % p;y = y >> 1;x = (x * x) % p;}return res;}
-int modadd(int a, int b) { return ((a % mod) + (b % mod)) % mod; }
-int modsub(int a, int b) { return ((a % mod) - (b % mod) + mod) % mod; }
-int modmul(int a, int b) { return ((1LL * a % mod) * (b % mod)) % mod; }
-uint modInverse(int n, int p=mod){return power(n, p - 2, p);}
-
-void solve(int t){
-    int n,k,res=0;
-    cin>>n>>k;
-    string str;
-    cin>>str;
-    for(int i=0;i<n;){
-        if(str[i] == 'W'){
-            i++;
-            continue;
-        }else{
-            res++;
-            i+=k;
-        }
-    }
-
-    cout<<res<<nl;
 }
 
-int32_t main(){
-    //init_code();
-    int t = 1,i=1;
-    cin >> t;
-    while (i<=t){
-        solve(i++);
+class cp {
+public:
+    ll a,b,n,m,x,y,l,k,r;
+    vector<ll>vec;
+    void solve(int t) {
+        int n,k;cin>>n>>k;
+        string str;cin>>str;
+        int tmp = 0,rs = 0;;
+
+        for(int i=0;i<n;i++){
+            if(str[i]=='W') continue;
+
+            rs++;
+            i+=(k-1);
+
+        }
+
+        cout<<rs<<nl;
+        
     }
+};
+
+int main() {
+    FAST_IO;
+    init_file_io();
+    cp obj;
+
+    int T=1;
+    cin>>T;
+    for(int t=1; t<=T; t++) obj.solve(t);
+    
     return 0;
 }
+
+
+/*
+  __  __       _           _                  _           
+ |  \/  |     | |         | |                (_)          
+ | \  / | ___ | |__   __ _| |_ __ _ _ __ ___  _ _ __ ___  
+ | |\/| |/ _ \| '_ \ / _` | __/ _` | '_ ` _ \| | '_ ` _ \ 
+ | |  | | (_) | | | | (_| | || (_| | | | | | | | | | | | |
+ |_|  |_|\___/|_| |_|\__,_|\__\__,_|_| |_| |_|_|_| |_| |_|
+                                                          
+*/
