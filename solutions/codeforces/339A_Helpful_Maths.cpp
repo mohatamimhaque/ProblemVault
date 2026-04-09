40a95c7c2b882cd31a8460d7ab2a807f66d1b925
Mohatamim Haque <mohatamimhaque@outlook.com>
2026-04-09 23:50:32 +0600

    Add/update 339A_Helpful_Maths.cpp

--- solutions/codeforces/339A_Helpful_Maths.cpp
+++ solutions/codeforces/339A_Helpful_Maths.cpp
@@ -1,35 +1,74 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <string>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    vector<int>vec;
-    string str;
-    cin>>str;
-    for(int i=0;i<str.size();i++){
-      if(i%2==0){
-        vec.push_back(str[i]-48);
-      }
-    }
-    sort(vec.begin(),vec.end());
-    int s=vec.size()-1;
-    for(int i=0;i<=s;i++){
-      cout<<vec.at(i);
-      if(i<s) cout<<'+';
-    }
-    cout<<endl;
-    
-    
-    
-    
-     
-    return 0;
-    
-    
-    
+#define FAST_IO ios::sync_with_stdio(false);cin.tie(nullptr)
+#define MOD 1000000007LL
+#define nl '\n'
 
-    
-    
+using ll=long long;
+using ull=unsigned long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+using vc=vector<ll>;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<"="<<x<<endl
+#define debugv(v) cerr<<#v<<"=";for(auto e:v) cerr<<e<<" ";cerr<<endl
+#else
+#define debug(x)
+#define debugv(v)
+#endif
+
+inline void init_file_io(){
+    #ifndef ONLINE_JUDGE 
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
 }
+
+class cp {
+public:
+    ll a,b,n,m,x,y,l,k,r;
+    vector<ll>vec;
+    void solve(int t) {
+        string str;cin>>str;
+        vector<int>vec;
+        for(auto v:str){
+            if(v=='+'){
+                continue;
+            }
+            vec.push_back(v - '0');
+        }
+
+        sort(vec.begin(),vec.end());
+        cout<<vec[0];
+        for(int i=1;i<vec.size();i++){
+            cout<<'+'<<vec[i];
+        }
+
+        cout<<nl;
+        
+    }
+};
+
+int main() {
+    FAST_IO;
+    init_file_io();
+    cp obj;
+
+    int T=1;
+    // cin>>T;
+    for(int t=1; t<=T; t++) obj.solve(t);
+    
+    return 0;
+}
+
+
+/*
+  __  __       _           _                  _           
+ |  \/  |     | |         | |                (_)          
+ | \  / | ___ | |__   __ _| |_ __ _ _ __ ___  _ _ __ ___  
+ | |\/| |/ _ \| '_ \ / _` | __/ _` | '_ ` _ \| | '_ ` _ \ 
+ | |  | | (_) | | | | (_| | || (_| | | | | | | | | | | | |
+ |_|  |_|\___/|_| |_|\__,_|\__\__,_|_| |_| |_|_|_| |_| |_|
+                                                          
+*/
